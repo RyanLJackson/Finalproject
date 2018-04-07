561ccba72536efd8adcd3074e0c5c74b83a00c6e
RyanLJackson <32985019+RyanLJackson@users.noreply.github.com>
2018-04-07 11:32:20 -0500

    Bones
    I added some of the bare bones of the

---
+++ Taxes.py
@@ -0,0 +1,136 @@
+'''
+Created on Mar 10, 2018
+
+@author: ITAUser
+'''
+def tax(filing_status, income):
+    if filing status == 'single':
+    elif filing_status == 'married filing jointly':
+        if (income >= 0) and (income <=9275)
+        tax = (0.1*income)
+   
+    if (income > 415050):
+        tax += (income-415050)(.396)
+        tax += 120529.75
+    elif(415050 >= income > 413350):
+        tax += (income-413350)(.35)
+        tax += 119934.75
+    elif(413350 >= income > 190150):
+        tax += (income-190150)(.33)
+        tax += 46278.75
+    elif(190150 >= income > 91150):
+        tax += (income-91150)(.28)
+        tax += 18558.75
+    elif(91150 >= income > 37650):
+        tax += (income-37650)(.25)
+        tax += 5183.75
+    elif(37650 >= income > 9275):
+        tax += (income-9275)(.15)
+        tax += 927.5
+    else:
+        tax = (income)*(.1)
+        
+if filling_status == "married filing jointly"
+    if (income >= 0) and (income <=9275)
+        tax = (0.1*income)
+   
+    if (income > 415050):
+        tax += (income-415050)(.396)
+        tax += 120529.75
+    elif(415050 >= income > 413350):
+        tax += (income-413350)(.35)
+        tax += 119934.75
+    elif(413350 >= income > 190150):
+        tax += (income-190150)(.33)
+        tax += 46278.75
+    elif(190150 >= income > 91150):
+        tax += (income-91150)(.28)
+        tax += 18558.75
+    elif(91150 >= income > 37650):
+        tax += (income-37650)(.25)
+        tax += 5183.75
+    elif(37650 >= income > 9275):
+        tax += (income-9275)(.15)
+        tax += 927.5
+    else:
+        tax = (income)*(.1)
+        
+if filling_status == "widover"
+    if (income >= 0) and (income <=9275)
+        tax = (0.1*income)
+   
+    if (income > 415050):
+        tax += (income-415050)(.396)
+        tax += 120529.75
+    elif(415050 >= income > 413350):
+        tax += (income-413350)(.35)
+        tax += 119934.75
+    elif(413350 >= income > 190150):
+        tax += (income-190150)(.33)
+        tax += 46278.75
+    elif(190150 >= income > 91150):
+        tax += (income-91150)(.28)
+        tax += 18558.75
+    elif(91150 >= income > 37650):
+        tax += (income-37650)(.25)
+        tax += 5183.75
+    elif(37650 >= income > 9275):
+        tax += (income-9275)(.15)
+        tax += 927.5
+    else:
+        tax = (income)*(.1)
+        
+if filling_status == "married filing separately":
+    if (income >= 0) and (income <=9275)
+        tax = (0.1*income)
+   
+    if (income > 415050):
+        tax += (income-415050)(.396)
+        tax += 120529.75
+    elif(415050 >= income > 413350):
+        tax += (income-413350)(.35)
+        tax += 119934.75
+    elif(413350 >= income > 190150):
+        tax += (income-190150)(.33)
+        tax += 46278.75
+    elif(190150 >= income > 91150):
+        tax += (income-91150)(.28)
+        tax += 18558.75
+    elif(91150 >= income > 37650):
+        tax += (income-37650)(.25)
+        tax += 5183.75
+    elif(37650 >= income > 9275):
+        tax += (income-9275)(.15)
+        tax += 927.5
+    else:
+        tax = (income)*(.1)
+        
+if filling_status == "head of household"
+
+        if (income >= 0) and (income <=9275)
+        tax = (0.1*income)
+   
+    if (income > 415050):
+        tax += (income-415050)(.396)
+        tax += 120529.75
+    elif(415050 >= income > 413350):
+        tax += (income-413350)(.35)
+        tax += 119934.75
+    elif(413350 >= income > 190150):
+        tax += (income-190150)(.33)
+        tax += 46278.75
+    elif(190150 >= income > 91150):
+        tax += (income-91150)(.28)
+        tax += 18558.75
+    elif(91150 >= income > 37650):
+        tax += (income-37650)(.25)
+        tax += 5183.75
+    elif(37650 >= income > 9275):
+        tax += (income-9275)(.15)
+        tax += 927.5
+    else:
+        tax = (income)*(.1)
+        
+def percent_of_tax(tax, income):
+def is_valid(filing_status, income):
+def main(filing_status, income):
