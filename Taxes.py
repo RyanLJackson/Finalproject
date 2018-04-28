5b9bf19fe6d037ccdb9a934b00c9fb49153412a2
RyanLJackson <32985019+RyanLJackson@users.noreply.github.com>
2018-04-28 11:17:06 -0500

    Finish

--- Taxes.py
+++ Taxes.py
@@ -25,29 +25,21 @@ def tax(filing_status, income):
     
         
 if filling_status == "married filing jointly"
-    if (income >= 0) and (income <=9275)
-        tax = (0.1*income)
    
-    if (income > 466951):
-        tax += (income-415050)(.396)
-        tax += 120529.75
-    elif(415050 >= income > 413351):
-        tax += (income-466950)(.35)
-        tax += 119934.75
-    elif(413350 >= income > 231451):
-        tax += (income-413350)(.33)
-        tax += 46278.75
-    elif(190150 >= income > 151901):
-        tax += (income-231450)(.28)
-        tax += 18558.75
-    elif(91150 >= income > 75301):
-        tax += (income-151900)(.25)
-        tax += 5183.75
-    elif(37650 >= income > 18550):
-        tax += (income-75300)(.15)
-        tax += 927.5
-    else:
-        tax = (income)*(.1)
+       if (income >= 0) and (income <=18550):
+            tax = (0.1*income)
+        elif (income > 18551) and (income <= 75300):
+            tax=(.1*18550+ 0.15*(income-18550))
+        elif (income > 75301) and (income<=151900):
+            tax=(.1*18550+ 0.15*(75300-18550)+.25*(income-75301))
+        elif (income > 151901) and (income <= 231450):
+            tax=(.1*18550+ 0.15*(75300-18550)+.25*(151900-75301)+.28*(income-151901))
+        elif (income > 231451) and (income<=413350):
+            tax=(.1*18550+ 0.15*(75300-18550)+.25*(151900-75301)+.28*(231450-151901)+.33(income-231451)) 
+        elif (income > 413351) and (income<=466950):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(91150-37650)+ .28*(190150-91151)+.33*(413350-190151)+.35*(income-466951))
+        elif (income > 466951):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(91150-37650)+ .28*(190150-91151)+.33*(413350-190151)+.35*(415050-466951)+.396*(income-466951))
         
 if filling_status == "widover"
     if (income >= 0) and (income <=9275)
@@ -73,68 +65,51 @@ if filling_status == "widover"
         tax += 927.5
         
 if filling_status == "married filing separately":
-    if (income >= 0) and (income <=9275)
-        tax = (0.1*income)
-   
-    if (income > 233476):
-        tax += (income-415050)(.396)
-        tax += 120529.75
-    elif(415050 >= income > 206676):
-        tax += (income-233475)(.35)
-        tax += 119934.75
-    elif(413350 >= income > 115726):
-        tax += (income-206675)(.33)
-        tax += 46278.75
-    elif(190150 >= income > 75951):
-        tax += (income-115725)(.28)
-        tax += 18558.75
-    elif(91150 >= income > 37650):
-        tax += (income-37650)(.25)
-        tax += 5183.75
-    elif(37650 >= income > 9275):
-        tax += (income-9275)(.15)
-        tax += 927.5
-    else:
-        tax = (income)*(.1)
+    
+         if (income >= 0) and (income <=9275):
+            tax = (0.1*income)
+        elif (income > 9275) and (income <= 37650):
+            tax=(.1*9275+ 0.15*(income-9275))
+        elif (income > 37650) and (income<=75950):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(income-37650))
+        elif (income > 75950) and (income <= 115725):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(75950-37650)+ .28*(income-75951))
+        elif (income > 115725) and (income<=206675):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(75950-37650)+ .28*(115725-74951)+.33*(income-115726)) 
+        elif (income > 206675) and (income<=233475):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(75950-37650)+ .28*(115725-74951)+.33*(206675-115726)+ .35(income-206675))
+        elif (income > 233476):
+            tax=(.1*9275+0.15*(37650-9275)+.25*(75950-37650)+ .28*(115725-74951)+.33*(206675-115726)+ .35(233475-206675)+.396(income-233476))
         
 if filling_status == "head of household"
 
-        if (income >= 0) and (income <=9275)
-        tax = (0.1*income)
-   
-    if (income > 441001):
-        tax += (income-415050)(.396)
-        tax += 120529.75)
-    elif(415050 >= income > 413351):
-        tax += (income-441001)(.35)
-        tax += 119934.75)
-    elif(413350 >= income > 210801):
-        tax += (income-413350)(.33)
-        tax += 46278.75)
-    elif(190150 >= income > ):
-        tax += (income-91150)(.28)
-        tax += 18558.75)
-    elif(91150 >= income > 50401):
-        tax += (income-130150)(.25)
-        tax += 5183.75)
-    elif(37650 >= income > 13251):
-        ta0x += (income-9275)(.15)
-        tax += 927.5-)
-    else:
-        tax = (income)*(.1)
+           if (income >= 0) and (income <=13250):
+            tax = (0.1*income)
+        elif (income > 13251) and (income <= 50400):
+            tax=(.1*13250+ 0.15*(income-13251))
+        elif (income > 50400) and (income<=130150):
+            tax=(.1*13250+0.15*(50400-13251)+.25*(income-50400))
+        elif (income > 130151) and (income <= 210800):
+            tax=(.1*13250+0.15*(50400-13251)+.25*(130150-50401)+ .28*(income-130151))
+        elif (income > 210801) and (income<=413350):
+            tax=(.1*13250+0.15*(50400-13251)+.25*(130150-50401)+ .28*(210800-130151)+.33*(income-210801)) 
+        elif (income > 413350) and (income<=441000):
+            tax=(.1*13250+0.15*(50400-13251)+.25*(130150-50401)+ .28*(210800-130515)+.33*(413350-210801)+ .35(income-413351))
+        elif (income > 441001):
+            tax=(.1*13250+0.15*(50400-13251)+.25*(130150-50401)+ .28*(210800-130515)+.33*(413350-210801)+ .35(441000-413351)+.396(income-441001))
+    return tax
         
-def percent_of_tax(tax, income):
-def is_valid(filing_status, income):
     
-    all_valid = True 
-    if(income < 0):
-        return False 
-    elif ((filing_status != "single")and 
-          (filing_status != "married filing jointly")
-          -)
-    
-    
-return true 
-    else 
-def main(filing_status, income):
-    print 
+def is_valid(filing_status,income):
+    if filing_status == 'single' or 'married filing jointly' or 'married filing separately' or 'Head of household':
+        if (income >=0):
+            return True
+        return False
+    return False
+def main():
+    user_input = input ("Filing status:")
+    user_input2 = (int)(input ("User income:"))
+    if is_valid(user_input,user_input2):
+        print(tax(user_input,user_input2))
+
+main()
